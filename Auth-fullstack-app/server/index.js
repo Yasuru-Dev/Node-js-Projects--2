9a3c2306d948ec8ee6e937b1b5e3f9b0fd6acdfc
YASURU2003 <yasurudanushka@gmail.com>
2026-05-17 22:22:33 +0530

    Second project first commit

---
+++ Auth-fullstack-app/server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+
+// MongoDB Connection
+mongoose.connect("mongodb://localhost:27017/auth-fullstack-app")
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+
+// Routes
+app.use(authRoutes);
+
+
+// Start Server
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
